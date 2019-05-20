5c78edc1a8412a1436420d1427ad21c1417d3cb9
LrE <lrespino@gmail.com>
2019-05-20 18:41:42 -0400

    eddited api

--- int.js
+++ int.js
@@ -1,38 +1,42 @@
 
 
-//Api for Archive parameter
+//Api for Archive example
  //var queryURL = "https://api.nytimes.com/svc/archive/v1/2019/1.json?api-key=QFRwGFnbnrvxELAMVuBjVu3IATnA4NGv";
- var archive = "https://api.nytimes.com/svc/archive/v1/"
+ //Api for search example 
+ //var queryURL = "https://api.nytimes.com/svc/archive/v1/2019/1.json?api-key=QFRwGFnbnrvxELAMVuBjVu3IATnA4NGv";
+ 
+ //Api for Archive parameter
+ //var archive = "https://api.nytimes.com/svc/archive/v1/"
  //Api fro search parameter
  var search = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="
 
- var inputYear = ""
- var inputSearch = ""
+ //var inputYear = ""
+ //var inputSearch = ""
  var key = "api-key=QFRwGFnbnrvxELAMVuBjVu3IATnA4NGv"
  
 
  //Calls an API when the search fuction is activated
- $("#inputSearch").on("click", function() {
- 
+ $("#btn btn-primary").on("click", function() {
+    
     var queryURL = search + inputSearch + key;
   $.ajax({
     url: queryURL,
     method: "GET"
   })
     .then(function(response) {
-
+        console.log(this)
     });
 });
 
 //calls an API when the search fuction is activated
-$("#inputArchive").on("click", function() {
+// $("#number-records").on("mousedown", function() {
  
-    var queryURL = archive + inputYear + key;
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-    .then(function(response) {
+//     var queryURL = archive + inputYear + key;
+//   $.ajax({
+//     url: queryURL,
+//     method: "GET"
+//   })
+//     .then(function(response) {
 
-    });
-});
+//     });
+// });
