11adff7e1006e89deea3dad170595d3fdcb0c275
LrE <lrespino@gmail.com>
2019-05-20 20:09:35 -0400

    update variables

--- int.js
+++ int.js
@@ -12,24 +12,30 @@
 
  //var inputYear = ""
  //var inputSearch = ""
- var key = "api-key=QFRwGFnbnrvxELAMVuBjVu3IATnA4NGv"
+ var key = "&api-key=QFRwGFnbnrvxELAMVuBjVu3IATnA4NGv"
  
  // INPUT ID #search-term (&q=#search-term)   #number-records (&hits=#number-records)    #begin-date(&pub_year=#begin-date)     #end-date(&pub_year=#end-date)
  //Calls an API when the search fuction is activated
- $("#submit-button").on("click", function() {
+ $("#submit-button").on("click", function(e) {
+     e.preventDefault();
     var searchTerm = $("#search-term").val();
     console.log(searchTerm)
-    var numRec = $("#number-records").val();
+    var numRec = "&hits=" + $("#number-records").val();
     console.log(numRec)
     var startYear = $("#begin-date").val();
+    console.log(startYear)
+    var endYear = $("#end-date").val();
+    console.log(endYear);
 
-    var queryURL = search + inputSearch + key;
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-    .then(function(response) {
-        console.log(this)
+    var queryURL = search + searchTerm + numRec + key;
+    console.log(queryURL);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+
     });
 });
 
